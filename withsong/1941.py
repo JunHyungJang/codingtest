60d2763e67c1c6c74ca9f819b2157eef095a9a07
junhyungjang <jsy3535@dgist.ac.kr>
2022-03-14 21:32:34 +0900

    20220314

--- withsong/1941.py
+++ withsong/1941.py
@@ -1,160 +1,44 @@
-# import sys
+matrix = [input() for _ in range(5)]
 
-# graph = []
-# for i in range(5):
-#     k = list((input()))
-#     graph.append(k)
+nx = [1,-1,0,0]
+ny = [0,0,1,-1]
 
-# visited = [[False]* 5 for i in range(5)]
+result_set = set()
 
-
-# cnt = 0
-# c_Y = 0
-# c_S = 0
-# answer = 0
-# lst = []
-# # print(graph)
-
-# def back(i,j):
-#     global cnt
-#     global c_S
-#     global c_Y
-#     global answer
-#     global lst
-
-#     dx = [1,-1,0,0]
-#     dy = [0,0,1,-1]
-#     # print(cnt,c_S)
-#     if cnt == 7:
-#         if c_S>=4:
-#             print(lst)
-#             answer+=1
-#         return
-    
-
-#     for k in range(4):
-#         nx = i + dx[k]
-#         ny = j + dy[k]
-
-#         if 0<=nx<5 and 0<=ny<5:
-#             if not visited[nx][ny]:
-#                 if graph[nx][ny] == "Y":
-#                     c_Y+=1
-#                     cnt+=1
-#                     lst.append((nx,ny))
-#                     back(nx,ny)
-#                     lst.remove((nx,ny))
-#                     cnt-=1
-#                     c_Y-=1
-#                 elif graph[nx][ny] == "S":
-#                     c_S+=1
-#                     cnt+=1
-#                     lst.append((nx,ny))
-#                     back(nx,ny)
-#                     lst.remove((nx,ny))
-#                     c_S-=1
-#                     cnt-=1
-
-
-# for i in range(5):
-#     for j in range(5):
-#         if graph[i][j] == 'S':
-#             c_S+=1
-#             cnt+=1
-#             back(i,j)
-#             c_S-=1
-#             cnt-=1
-#         elif graph[i][j] == "Y":
-#             c_Y+=1
-#             cnt+=1
-#             back(i,j)
-#             c_Y-=1
-#             cnt-=1
-#         visited[i][j] = True
-
-
-# print(answer)
-
-graph = []
-
-for i in range(5):
-    k = list(input())
-    graph.append(k)
-
-
-answer = 0
-
-gvisited = [[False] * 5 for i in range(5)]
-
-lst = []
-
-def bfs(i,j):
-    global answer
-    global cnt
-    global c_S
-    global c_Y
-    dx = [1,-1,0,0]
-    dy = [0,0,1,-1]
-    if cnt == 7:
-        if lst[0] == (1,0):
-            print(lst)
-            print(c_S)
-
-        if c_S>=4:
-            answer+=1
+def backtrack(arr,index = 0, S = 0, Y = 0):
+    tmp = arr
+    if Y> 3:
         return
+    if index == 6:
+        arr.sort()
+        result_set.add(tuple(arr))
     
+    else:
+        adjacents = []
+        for node in range(len(arr)):
+            for i in range(4):
+                dx = arr[node][0] + nx[i]
+                dy = arr[node][1] + ny[i]
 
-    for k in range(4):
-        nx = i + dx[k]
-        ny = j + dy[k]
+                if 0> dx or 5<= dx or 0>dy or 5<= dy :
+                    continue
+                if (dx,dy) in arr:
+                    continue
+                adjacents.append((dx,dy))
+        
+        for adjacent in adjacents:
+            kx = adjacent[0]
+            ky = adjacent[1]
 
-        if 0<=nx<5 and 0<=ny<5:
-            if not visited[nx][ny] and not gvisited[nx][ny]:
-                if graph[nx][ny] == "Y":
-                    c_Y+=1
-                    cnt+=1
-                    visited[nx][ny] = True
-                    lst.append((nx,ny))
-                    bfs(nx,ny)
-                    lst.remove((nx,ny))
-
-                    visited[nx][ny] = False
-                    cnt-=1
-                    c_Y-=1
-                elif graph[nx][ny] == "S":
-                    c_S+=1
-                    cnt+=1
-                    lst.append((nx,ny))
-                    visited[nx][ny] = True
-
-                    bfs(nx,ny)
-                    visited[nx][ny] = False
-                    lst.remove((nx,ny))
-                    c_S-=1
-                    cnt-=1
-
-    
+            if matrix[kx][ky] == "S":
+                backtrack(arr + [(kx,ky)], index+1,S+1, Y)
+            else:
+                backtrack(arr + [(kx,ky)], index+1,S,Y+1)
     
 
 for i in range(5):
     for j in range(5):
-        cnt = 0
-        c_Y = 0
-        c_S = 0
+        if matrix[i][j] == 'S':
+            backtrack([(i,j)],index = 0, S= 1)
 
-        visited = [[False] * 5 for i in range(5)]
-        visited[i][j] = True
-        cnt+=1
-        lst.append((i,j))
-        if graph[i][j] == 'Y':
-            c_Y+=1
-        elif graph[i][j] == 'S':
-            c_S+=1
-        bfs(i,j)
-        lst.remove((i,j))
-        
-        gvisited[i][j] = True
-
-
-print(answer)
+print(len(result_set))
